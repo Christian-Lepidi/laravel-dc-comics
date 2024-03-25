4f8cd211f2fbec10d8b3e7191563ac6043fd8baa
Christian-Lepidi <christianlepidi@gmail.com>
2024-03-25 21:43:48 +0100

    create table but view not found

--- resources/views/comics.blade.php/index.blade.php
+++ resources/views/comics.blade.php/index.blade.php
@@ -6,6 +6,43 @@
   <section>
     <div class="container py-4">
       <h1 class="text-center">Lista Comics</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Titolo</th>
+            <th>Descrizione</th>
+            <th>Prezzo</th>
+            <th>>Serie</th>
+            <th>Data d'uscita</th>
+            <th>Tipologia</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          @forelse ($comics as $comic)
+          <tr>
+            <td>{{$comic->id}}</td>
+            <td>{{$comic->title}}</td>
+            <td>{{$comic->description}}</td>
+            <td>{{$comic->price}}</td>
+            <td>{{$comic->series}}</td>
+            <td>{{$comic->sale_date}}</td>
+            <td>{{$comic->type}}</td>
+            <td></td>
+          </tr>  
+          @empty
+          <tr>
+           <td colspan="100%">
+            Nessun risultato
+           </td> 
+          </tr>    
+          @endforelse
+          
+
+          
+        </tbody>
+      </table>
     </div>
   </section>
 @endsection
