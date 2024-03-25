a2ce285d8e4f8a9c910db22aa7d776e0bc049f7c
Christian-Lepidi <christianlepidi@gmail.com>
2024-03-25 17:53:41 +0100

    feat: add. db table and view comics in index

---
+++ resources/views/comics.blade.php/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Comics')
+
+@section('main-content')
+  <section>
+    <div class="container py-4">
+      <h1 class="text-center">Lista Comics</h1>
+    </div>
+  </section>
+@endsection
